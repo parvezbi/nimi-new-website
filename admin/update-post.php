97a9dfdbb40eca93bd50da622829170aec5051ce
Md Parvez Habib <52429474+parvezbi@users.noreply.github.com>
2022-10-20 01:20:56 +0600

    Update update-post.php

--- admin/update-post.php
+++ admin/update-post.php
@@ -15,7 +15,7 @@
   <div class="container">
   <div class="row">
     <div class="col-md-12">
-        <h1 class="admin-heading">Update Post</h1>
+        <h1 class="admin-heading" style="text-align: center;">Update Post</h1>
     </div>
     <div class="col-md-offset-3 col-md-6">
     <?php 
